f8c7d953a0bc92e9b87cfa66cd6fc799bba5a180
mdmarufsarker <mdmarufsarker2021@gmail.com>
2023-08-23 02:57:08 +0600

    footer section updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,11 @@ export default function Footer({ title }: FooterProps) {
   return (
     <>
       <footer className="py-4">
-        <h1 className="text-center text-xl lgl:text-3xl pt-8 font-bold">
+        <h1 className="text-center xs:text-sm sm:text-lg md:text-xl lgl:text-3xl pt-8 font-bold">
           {currentYear} © <span className="text-red-500">Stack Learner </span>
           {title}
         </h1>
-        <p className="text-center text-xl">
+        <p className="text-center xs:text-sm md:text-lg">
           Contact:{" "}
           <a href="mailto:support@stacklearner.com" className="cursor-pointer">
             support@stacklearner.com
